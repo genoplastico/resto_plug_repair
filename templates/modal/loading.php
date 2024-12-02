a1c84d6924c0900b8227f7ae474b8dc5f46f1b4a
genoplastico <95545065+genoplastico@users.noreply.github.com>
2024-12-01 22:28:20 -0300

    Delete templates directory

--- templates/modal/loading.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('ABSPATH')) exit; ?>
-
-<div class="arm-modal-loading">
-    <div class="arm-modal-loading-spinner"></div>
-    <div class="arm-modal-loading-text">
-        <?php echo esc_html($message ?? __('Loading...', 'appliance-repair-manager')); ?>
-    </div>
-</div>

344cf7efddcd376b875e922372cd4247af68e546
genoplastico <95545065+genoplastico@users.noreply.github.com>
2024-12-02 01:01:32 -0300

    Commit from Bolt to GitHub

--- includes/Core/Activator.php
+++ includes/Core/Activator.php
@@ -26,6 +26,17 @@ class Activator {
             KEY attachment_id (attachment_id)
         ) $charset_collate;";
 
+        // Appliance Images table
+        $sql_appliance_images = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}arm_appliance_images (
+            id bigint(20) NOT NULL AUTO_INCREMENT,
+            appliance_id bigint(20) NOT NULL,
+            attachment_id bigint(20) NOT NULL,
+            created_at datetime DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY  (id),
+            KEY appliance_id (appliance_id),
+            KEY attachment_id (attachment_id)
+        ) $charset_collate;";
+
         // Clients table
         $sql_clients = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}arm_clients (
             id bigint(20) NOT NULL AUTO_INCREMENT,
@@ -93,7 +104,6 @@ class Activator {
         dbDelta($sql_appliances);
         dbDelta($sql_repairs);
         dbDelta($sql_repair_notes);
-        dbDelta($sql_appliance_images);
         dbDelta($sql_appliance_images);
     }
 
